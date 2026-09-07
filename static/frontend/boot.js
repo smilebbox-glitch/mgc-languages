71d7f7c058129b75a2df5a7f9e18bc3dcf77f211
smilebbox-glitch <smilebbox@gmail.com>
2026-09-07 21:20:56 +0300

    v6.0.11: fix boot error fallback escaping

--- static/frontend/boot.js
+++ static/frontend/boot.js
@@ -48,7 +48,8 @@
     if (main) {
       main.innerHTML = '<div class="card"><h2>Не удалось загрузить интерфейс</h2><p>' +
         String(message).replace(/[&<>"']/g, function (char) {
-          return {'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#039;'}[char] + '</p></div>';
+          return {'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#039;'}[char];
+        }) + '</p></div>';
     }
     throw error;
   }
