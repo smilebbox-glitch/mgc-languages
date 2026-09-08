57999d6e97756c6819cf88d5a92bfafe611e31e6
smilebbox-glitch <smilebbox@gmail.com>
2026-09-08 21:27:14 +0300

    chore: require arcade mastery module at boot

--- static/frontend/boot.js
+++ static/frontend/boot.js
@@ -18,6 +18,7 @@
       'game-engagement-v618',
       'factory-journey-v619',
       'arcade-missions-v620',
+      'arcade-mastery-v620',
       'practice-games',
       'support-notifications',
       'assistant-knowledge',
@@ -55,7 +56,7 @@
     if (main) {
       main.innerHTML = '<div class="card"><h2>Не удалось загрузить интерфейс</h2><p>' +
         String(message).replace(/[&<>"']/g, function (char) {
-          return {'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#039;'}[char];
+          return {'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot',"'":'&#039;'}[char];
         }) + '</p></div>';
     }
     throw error;
